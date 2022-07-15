74dd275c2e822101418e20261863bb5a4467b290
Marcos Willian <willian.marcos7@hotmail.com>
2022-07-15 20:20:08 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
+  return (
     <>
-    <h1>Hello, World!</h1>
-    
+      <h1>Hello, World!</h1>
+      <NotificationButton />
+      <NotificationButton />
     </>
   )
 }
