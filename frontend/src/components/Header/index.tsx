7b7c301774a4c9ad8328ecc27fce9d558972d4ea
Marcos Willian <willian.marcos7@hotmail.com>
2022-07-15 22:53:06 -0300

    Add useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import logo from '../../assets/img/logo.svg'
-import './styles.css'
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
 
 function Header() {
     return (
@@ -7,12 +7,12 @@ function Header() {
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="Logo_DSMeta" />
                     <h1>DSMeta</h1>
-                    <p>Desenvolvido por:
-                        <a href="https://www.instagram.com/marcos.wgpersico/">@marcos.wgpersico</a>
+                    <p>Desenvolvido por: 
+                        <a href="https://www.instagram.com/marcos.wgpersico/"> @marcos.wgpersico</a>
                     </p>
             </div>
         </header>
     )
 }
 
-export default Header
+export default Header;
